f8dc684c7261d0e3061a1ff985784f11e7b2599b
Devanshu Joshi <iamdevanshu007@gmail.com>
2025-06-07 11:14:43 +0530

    Added Cross Origin Annotations

--- src/main/java/com/example/Starter/StarterApplication.java
+++ src/main/java/com/example/Starter/StarterApplication.java
@@ -2,8 +2,10 @@ package com.example.Starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin
 public class StarterApplication {
 
 	public static void main(String[] args) {

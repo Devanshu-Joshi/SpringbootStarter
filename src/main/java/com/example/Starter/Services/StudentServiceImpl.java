9c30ee3d69a12cbb9282d9e8860a1ddd1b799fbd
Devanshu Joshi <iamdevanshu007@gmail.com>
2025-06-07 10:01:42 +0530

    Sixth - Added Implemented Code

--- src/main/java/com/example/Starter/Services/StudentServiceImpl.java
+++ src/main/java/com/example/Starter/Services/StudentServiceImpl.java
@@ -1,7 +1,29 @@
 package com.example.Starter.Services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
-public class StudentServiceImpl extends StudentService {
+public class StudentServiceImpl implements StudentService {
+    List<String> students = new ArrayList<>();
+
+    String student1 = "Narendra Modi";
+    String student2 = "Mukesh Ambani";
+    String student3 = "Elon Musk";
+
+    public StudentServiceImpl() {
+        students.add(student1);
+        students.add(student2);
+        students.add(student3);
+    }
+
+    public List<String> getAllStudents() {
+        return students;
+    }
+
+    public void setStudent(String student) {
+        students.add(student);
+    }
 }

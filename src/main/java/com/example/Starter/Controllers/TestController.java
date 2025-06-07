81b5c8f5cc59117220759fe856019b8062d9f1a9
Devanshu Joshi <iamdevanshu007@gmail.com>
2025-06-07 10:06:28 +0530

    Eighth - Autowired

--- src/main/java/com/example/Starter/Controllers/TestController.java
+++ src/main/java/com/example/Starter/Controllers/TestController.java
@@ -1,6 +1,7 @@
 package com.example.Starter.Controllers;
 
 import com.example.Starter.Services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -8,6 +9,7 @@ import java.util.List;
 @RestController
 public class TestController {
 
+    @Autowired
     StudentService StudentService;
 
     @GetMapping("/hello")

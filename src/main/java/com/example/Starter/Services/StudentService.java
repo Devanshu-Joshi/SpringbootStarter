ef9dcd50ef1aaec4ff136f8724b8d0be51fbae14
Devanshu Joshi <iamdevanshu007@gmail.com>
2025-06-07 10:56:51 +0530

    Added Service Methods

--- src/main/java/com/example/Starter/Services/StudentService.java
+++ src/main/java/com/example/Starter/Services/StudentService.java
@@ -1,9 +1,17 @@
 package com.example.Starter.Services;
 
+import com.example.Starter.Models.Student;
+
 import java.util.List;
 
 public interface StudentService {
-    List<String> getAllStudents();
+    List<Student> getAllStudents();
 
-    void setStudent(String student);
+    Student getStudentByID(int id);
+
+    Student setStudent(Student Student);
+
+    Student updateStudent(Student Student);
+
+    Student deleteStudent(int id);
 }

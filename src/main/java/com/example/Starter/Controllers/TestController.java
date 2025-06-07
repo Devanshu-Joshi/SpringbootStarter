34cfbdf555d561429b06b81c8f1eb57f575c4511
Devanshu Joshi <iamdevanshu007@gmail.com>
2025-06-07 10:05:23 +0530

    Seventh - Some code in TestController

--- src/main/java/com/example/Starter/Controllers/TestController.java
+++ src/main/java/com/example/Starter/Controllers/TestController.java
@@ -1,14 +1,23 @@
 package com.example.Starter.Controllers;
 
+import com.example.Starter.Services.StudentService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 public class TestController {
+
+    StudentService StudentService;
 
     @GetMapping("/hello")
     public String hello()
     {
         return "Hello World";
+    }
+
+    @GetMapping("/getAllStudents")
+    public List<String> getAll(){
+        // ??
     }
 }

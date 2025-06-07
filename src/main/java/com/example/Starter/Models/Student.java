6238fcb6f517ee343831f4d161aef4e936422659
Devanshu Joshi <iamdevanshu007@gmail.com>
2025-06-07 10:31:17 +0530

    Added GetSet

--- src/main/java/com/example/Starter/Models/Student.java
+++ src/main/java/com/example/Starter/Models/Student.java
@@ -24,4 +24,36 @@ public class Student {
         this.city = city;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
 }

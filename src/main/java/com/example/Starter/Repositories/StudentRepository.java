d3b1d12b46f751f61445c701f9e384fe5a4338e6
Devanshu Joshi <iamdevanshu007@gmail.com>
2025-06-07 10:34:05 +0530

    Extends Repository

--- src/main/java/com/example/Starter/Repositories/StudentRepository.java
+++ src/main/java/com/example/Starter/Repositories/StudentRepository.java
@@ -1,4 +1,7 @@
 package com.example.Starter.Repositories;
 
-public interface StudentRepository {
+import com.example.Starter.Models.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Integer> {
 }

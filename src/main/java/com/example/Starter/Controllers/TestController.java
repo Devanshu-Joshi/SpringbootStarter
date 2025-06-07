76a710f2054f4d6b6b3dbc54285b6b967d85ab87
Devanshu Joshi <iamdevanshu007@gmail.com>
2025-06-07 11:01:55 +0530

    Adding Methods in TestController

--- src/main/java/com/example/Starter/Controllers/TestController.java
+++ src/main/java/com/example/Starter/Controllers/TestController.java
@@ -1,11 +1,12 @@
 package com.example.Starter.Controllers;
 
+import com.example.Starter.Models.Student;
 import com.example.Starter.Services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -20,13 +21,47 @@ public class TestController {
         return "Hello World";
     }
 
-    @GetMapping("/getAllStudents")
-    public List<String> getAll(){
-        return studentService.getAllStudents();
+    @GetMapping("/Students")
+    public ResponseEntity<List<Student>> getStudents() {
+        List<Student> list = studentService.getAllStudents();
+
+        if (list.size() <= 0) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(list);
     }
 
-    @PostMapping("/students")
-    public void setStudent(@RequestBody String student) {
-        studentService.setStudent(student);
+    @GetMapping("/Students/{id}")
+    public ResponseEntity<Student> getStudentByID(@PathVariable("id") int id) {
+        Student Student = studentService.getStudentByID(id);
+        if (Student == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(Student);
+    }
+
+    @PostMapping("/Students")
+    public ResponseEntity<Student> setStudent(@RequestBody Student u) {
+        Student Student = studentService.setStudent(u);
+        if (Student == null)
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        return ResponseEntity.status(HttpStatus.CREATED).body(Student);
+    }
+
+    @PutMapping("/Students")
+    public ResponseEntity<Student> updateStudent(@RequestBody Student u) {
+        Student Student = studentService.updateStudent(u);
+        if (Student == null)
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        return ResponseEntity.status(HttpStatus.CREATED).body(Student);
+    }
+
+    @DeleteMapping("/Students/{id}")
+    public ResponseEntity<Student> deleteStudent(@PathVariable("id") int id) {
+        Student Student = studentService.deleteStudent(id);
+        if (Student == null)
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        return ResponseEntity.status(HttpStatus.CREATED).body(Student);
     }
 }

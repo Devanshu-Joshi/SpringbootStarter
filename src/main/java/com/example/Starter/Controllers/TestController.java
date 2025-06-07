8253a363c205716450b79869af5b31a51a9cc33c
Devanshu Joshi <iamdevanshu007@gmail.com>
2025-06-07 10:11:17 +0530

    Added Post Method

--- src/main/java/com/example/Starter/Controllers/TestController.java
+++ src/main/java/com/example/Starter/Controllers/TestController.java
@@ -3,6 +3,8 @@ package com.example.Starter.Controllers;
 import com.example.Starter.Services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
@@ -21,5 +23,10 @@ public class TestController {
     @GetMapping("/getAllStudents")
     public List<String> getAll(){
         return studentService.getAllStudents();
+    }
+
+    @PostMapping("/students")
+    public void setStudent(@RequestBody String student) {
+        studentService.setStudent(student);
     }
 }

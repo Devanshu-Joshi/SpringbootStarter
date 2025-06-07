1cb5dc21a54062e0611fe61438eef0552948f1db
Devanshu Joshi <iamdevanshu007@gmail.com>
2025-06-07 10:52:41 +0530

    Added one method

--- src/main/java/com/example/Starter/Repositories/StudentRepository.java
+++ src/main/java/com/example/Starter/Repositories/StudentRepository.java
@@ -4,4 +4,6 @@ import com.example.Starter.Models.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository<Student, Integer> {
+    // All Methods are Here , Except One
+    Student findById(int id);
 }
